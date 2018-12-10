0fc2efa6131eabbdf123ceecbfce8308edeab577
galaxymacos <galaxymacos@gmail.com>
2018-12-10 15:30:27 -0500

    Initial commit

---
+++ Assets/Scripts/ActivateChannel.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+public class ActivateChannel : MonoBehaviour {
+
+	[SerializeField] private AudioMixer audioM;
+
+	[SerializeField] private string nameParam;
+	
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	private void OnTriggerEnter(Collider other) {
+		if (other.gameObject.CompareTag("Player")) {
+			audioM.SetFloat(nameParam, 0f);
+		}
+	}
+
+	private void OnTriggerExit(Collider other) {
+		if (other.gameObject.CompareTag("Player")) {
+			audioM.SetFloat(nameParam, -80f);
+		}
+	}
+}
---
+++ Assets/SwitchSnapshot.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+
+public class SwitchSnapshot : MonoBehaviour {
+
+	[SerializeField] private AudioMixerSnapshot outside;
+	[SerializeField] private AudioMixerSnapshot inside;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	private void OnTriggerEnter(Collider other) {
+		if (other.gameObject.CompareTag("Player")) {
+			inside.TransitionTo(0.1f);
+            print("Player enters the zone");
+		}
+	}
+
+	private void OnTriggerExit(Collider other) {
+		if (other.gameObject.CompareTag("Player")) {
+			outside.TransitionTo(0.1f);
+            print("Player leavess the zone");
+
+        }
+    }
+}
